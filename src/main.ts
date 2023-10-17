573052456a65f597212732f2876859dca842393b
Robinson De La Cruz <rdelacruzsiado@gmail.com>
2023-10-16 22:29:56 -0500

    feat: add documentation with swagger and some project configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Vass API')
+    .setVersion('1.0')
+    .setDescription(
+      'The User API allows clients to manage user-related operations. The auth endpoints enable clients to register and log in. The customer endpoints are designed for clients to manage customer information.',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(cookieParser());
+
+  // Strip any properties from the incoming data that do not have corresponding properties in the DTO class.
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('APP_PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();
